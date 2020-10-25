731ed637c58f6952dbd91b6d26353c88e2003fd7
Ryan Wilcox <rwilcox11@icloud.com>
2020-10-25 12:48:13 -0400

    test update

--- src/test/java/com/example/demo/TestUtils.java
+++ src/test/java/com/example/demo/TestUtils.java
@@ -11,7 +11,7 @@ public class TestUtils {
     try {
       Field f = target.getClass().getDeclaredField(fieldName);
 
-      if (!f.canAccess(target)) {
+      if (!f.isAccessible()) {
         f.setAccessible(true);
         wasPrivate = true;
       }
